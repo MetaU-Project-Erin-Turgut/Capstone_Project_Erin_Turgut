05ea7cc40017b1733839527ec621cba44465af0e
Erin Turgut <erintur@meta.com>
2025-07-10 20:56:26 -0700

    central coordinates for group recalculated in user drop group endpoint

--- server/routes/groups.js
+++ server/routes/groups.js
@@ -6,20 +6,20 @@ const prisma = new PrismaClient();
 
 const { isAuthenticated } = require('../middleware/CheckAutheticated')
 const { findGroups } = require('../systems/GroupFindAlgo');
-const { updateGroupCentralLocation, updateGroupInterests } = require('../systems/GroupUpdateMethods')
+const { updateGroupCentralLocation, updateGroupInterests, recalculateGroupCentralLocation } = require('../systems/GroupUpdateMethods')
 const { Status, filterMembersByStatus } = require('../systems/Utils');
 
-const FULL_GROUP_SIZE = 3;
+const FULL_GROUP_SIZE = 10;
 
 //get user's groups 
 router.get('/user/groups/', isAuthenticated, async (req, res) => {
     try {
         const userData = await prisma.user.findUnique({
             where: {
-                id: req.session.userId, 
+                id: req.session.userId,
             },
-            include: { 
-                groups: {include: {group: { include: {interests: {include: {interest: true}}, members: {where: {NOT: {user_id: req.session.userId}}, include: {user: true}}}}}}
+            include: {
+                groups: { include: { group: { include: { interests: { include: { interest: true } }, members: { where: { NOT: { user_id: req.session.userId } }, include: { user: true } } } } } }
             }
         })
         res.status(201).json(userData.groups)
@@ -32,7 +32,7 @@ router.get('/user/groups/', isAuthenticated, async (req, res) => {
 //update user's status for a group
 router.patch('/user/groups/:groupId/status', isAuthenticated, async (req, res) => {
     const groupId = parseInt(req.params.groupId);
-    const {updatedStatus} = req.body; 
+    const { updatedStatus } = req.body;
 
     try {
         const isGroup_user = await prisma.group_User.findUnique({
@@ -45,7 +45,7 @@ router.patch('/user/groups/:groupId/status', isAuthenticated, async (req, res) =
         });
 
         if (!isGroup_user) {
-            res.status(404).json({error: 'This user - group relationship does not exist'});
+            res.status(404).json({ error: 'This user - group relationship does not exist' });
         }
 
         const updatedGroup = await prisma.group_User.update({
@@ -70,16 +70,16 @@ router.patch('/user/groups/:groupId/status', isAuthenticated, async (req, res) =
 
 //get new group suggestions - will be called immediately after user updates their interests
 router.get('/user/groups/new', isAuthenticated, async (req, res) => {
-    
+
 
     try {
         const updatedUser = await prisma.user.findUnique({
-                where: {id: req.session.userId},
-                select: {id: true, interests: true}
+            where: { id: req.session.userId },
+            select: { id: true, interests: true }
         });;
         //Call GroupFindAlgo, will return all suggested groups sorted by compatibility
         const suggestedGroups = await findGroups(updatedUser); //NOTE: the interests attribute part of the returned objects won't be entirely accurate - not needed for now though
-        
+
         //update the user_group table with these invites
         for (group of suggestedGroups) {
 
@@ -96,16 +96,16 @@ router.get('/user/groups/new', isAuthenticated, async (req, res) => {
             if (!isGroup_user) {
                 await prisma.group_User.create({
                     data: {
-                        group: {connect: {id: group.id} },
-                        user: {connect: {id: req.session.userId} },
-                        status: Status.PENDING, 
-                        compatibilityRatio: group.compatibilityRatio 
+                        group: { connect: { id: group.id } },
+                        user: { connect: { id: req.session.userId } },
+                        status: Status.PENDING,
+                        compatibilityRatio: group.compatibilityRatio
                     }
                 })
             }
-            
+
         }
-        
+
         res.status(201).json(suggestedGroups)
     } catch (error) {
         console.error("Error fetching groups:", error)
@@ -116,87 +116,85 @@ router.get('/user/groups/new', isAuthenticated, async (req, res) => {
 //accept group - first, the endpoint above will be called, then this endpoint will be called
 //update user's status for a group
 router.put('/user/groups/:groupId/accept', isAuthenticated, async (req, res) => {
-   const groupId = parseInt(req.params.groupId);
+    const groupId = parseInt(req.params.groupId);
 
 
-   try {
+    try {
         //get group and user relationship and each of their coordinates
-       const group_user = await prisma.group_User.findUnique({
-           where: {
-               user_id_group_id: {
-                   group_id: groupId,
-                   user_id: req.session.userId
-               }
-           },
-           include: {
-               group: { include: {interests: {select: {interest: true}}, members: true}},
-               user: { include: {interests: true} }
-           }
-       });
-       const userCoord = await prisma.$queryRaw`
+        const group_user = await prisma.group_User.findUnique({
+            where: {
+                user_id_group_id: {
+                    group_id: groupId,
+                    user_id: req.session.userId
+                }
+            },
+            include: {
+                group: { include: { interests: { select: { interest: true } }, members: true } },
+                user: { include: { interests: true } }
+            }
+        });
+        const userCoord = await prisma.$queryRaw`
         SELECT ST_X(coord::geometry), ST_Y(coord::geometry)
         FROM "User" 
         WHERE id = ${req.session.userId}`;
-       const groupCoord = await prisma.$queryRaw`
+        const groupCoord = await prisma.$queryRaw`
         SELECT ST_X(coord::geometry), ST_Y(coord::geometry)
         FROM "Group" 
         WHERE id = ${groupId}`;
 
+        if (!group_user) {
+            res.status(404).json({ error: 'This user - group relationship does not exist' });
+        }
 
+        //now that user accepted this group, we need to update the group's interest list and central location
 
-       if (!group_user) {
-           res.status(404).json({error: 'This user - group relationship does not exist'});
-       }
+        const newGroupCoords = updateGroupCentralLocation(groupCoord.at(0), userCoord.at(0));
+        const newGroupInterests = await updateGroupInterests(group_user.group.interests, group_user.user.interests);
 
-       //now that user accepted this group, we need to update the group's interest list and central location
+        const acceptedMembers = filterMembersByStatus(group_user.group.members, Status.ACCEPTED);
+        let newIsFullStatus = false;
 
-       const newGroupCoords = updateGroupCentralLocation(groupCoord.at(0), userCoord.at(0));
-       const newGroupInterests = await updateGroupInterests(group_user.group.interests, group_user.user.interests);
-
-       const acceptedMembers = filterMembersByStatus(group_user.group.members, Status.ACCEPTED);
-       let newIsFullStatus = false;
-
-       if (acceptedMembers.length + 1 >= FULL_GROUP_SIZE) {
+        if (acceptedMembers.length + 1 >= FULL_GROUP_SIZE) {
             newIsFullStatus = true;
-       }
-       
-       //update group in database (update if is now full, update coordinates, update interests list)
-       const updatedGroup = await prisma.group.update({
-            where: {id: groupId},
+        }
+//
+        //update group in database (update if is now full, update coordinates, update interests list)
+        const updatedGroup = await prisma.group.update({
+            where: { id: groupId },
             data: {
-                 interests: {
+                interests: {
                     deleteMany: {},
                     create: newGroupInterests.map((interest) => {
-                        return { interest: { connect: {id: interest} } }
+                        return { interest: { connect: { id: interest } } }
                     })
-                 },
-                 is_full: newIsFullStatus
-             },
-       })
+                },
+                is_full: newIsFullStatus
+            },
+        })
 
-       //update group coordinates
-       await prisma.$queryRaw`
-       UPDATE "Group" 
-       SET "coord" = (ST_SetSRID(ST_MakePoint(${newGroupCoords.longitude}, ${newGroupCoords.latitude}), 4326)::geography)
-       WHERE id=${groupId}`;
+        //update group coordinates
+        await prisma.$queryRaw`
+        UPDATE "Group" 
+        SET "coord" = (ST_SetSRID(ST_MakePoint(${newGroupCoords.st_x}, ${newGroupCoords.st_y}), 4326)::geography)
+        WHERE id=${groupId}`;
 
 
-       //update status of group_user relationship
-       const updatedGroupUser = await prisma.group_User.update({
-           where: {
-               user_id_group_id: {
-                   group_id: groupId,
-                   user_id: req.session.userId
-               }
-           },
-           data: {
-               status: Status.ACCEPTED 
-           },
-           include: {group: {include: {interests: {include: {interest: true}}, members: {where: {NOT: {user_id: req.session.userId}}, include: {user: true}}}}}
-       })
+        //update status of group_user relationship
+        const updatedGroupUser = await prisma.group_User.update({
+            where: {
+                user_id_group_id: {
+                    group_id: groupId,
+                    user_id: req.session.userId
+                }
+            },
+            data: {
+                status: Status.ACCEPTED
+            },
+            include: { group: { include: { interests: { include: { interest: true } }, members: { where: { NOT: { user_id: req.session.userId } }, include: { user: true } } } } }
+        })
 
-       //if group is now full, remove it as a "Pending" option from users' lists who have not accepted the group
-       if (newIsFullStatus) {
+        //if group is now full, remove it as a "Pending" option from users' lists who have not accepted the group
+        if (newIsFullStatus) {
             await prisma.group.update({
                 where: {
                     id: groupId
@@ -209,15 +207,73 @@ router.put('/user/groups/:groupId/accept', isAuthenticated, async (req, res) => 
                     }
                 }
             });
-       }
+        }
 
 
-       res.status(200).json(updatedGroupUser);
+        res.status(200).json(updatedGroupUser);
 
 
-   } catch (error) {
-       console.error("Error updating group:", error)
-       res.status(500).json({ error: "Could not update your response to this group." });
-   }
+    } catch (error) {
+        console.error("Error updating group:", error)
+        res.status(500).json({ error: "Could not update your response to this group." });
+    }
 })
+
+//drop group
+router.put('/user/groups/:groupId/drop', isAuthenticated, async (req, res) => {
+    const groupId = parseInt(req.params.groupId);
+    try {
+        //get group and user relationship
+        const group_user = await prisma.group_User.findUnique({
+            where: {
+                user_id_group_id: {
+                    group_id: groupId,
+                    user_id: req.session.userId
+                }
+            },
+            include: {
+                group: { include: { interests: { select: { interest: true } }, members: { where: { NOT: { user_id: req.session.userId } } } }},
+                user: { include: { interests: true } }
+            }
+        });
+
+        if (!group_user) {
+            res.status(404).json({ error: 'This user - group relationship does not exist' });
+        }
+        
+        //filter to only users that actaully accepted the group
+        const acceptedMembers = group_user.group.members.filter((user) => {
+            return user.status === Status.ACCEPTED;
+        })
+ 
+        //TODO: update group interests with member removal
+        //if group is now empty with removal of this user, remove the group
+        if (acceptedMembers.length === 0) { //remember, user is already excluded for list from db query - so should check against 0
+            await prisma.group.delete({
+                where: { id: groupId }
+            })
+        } else {
+            //recalculate group's central location
+
+            const memberIds = acceptedMembers.map((member) => {
+                return member.user_id
+            })
+
+            const memberCoords = await prisma.$queryRaw`
+            SELECT ST_X(coord::geometry), ST_Y(coord::geometry)
+            FROM "User" 
+            WHERE id = ANY(${(memberIds)})`;
+
+            const newGroupCoords = await recalculateGroupCentralLocation(memberCoords);
+
+        }
+        res.status(200).json({});
+
+    } catch (error) {
+        console.error("Error updating group:", error)
+        res.status(500).json({ error: "Could not update your response to this group." });
+    }
+})
+
+
 module.exports = router
--- server/systems/GroupUpdateMethods.js
+++ server/systems/GroupUpdateMethods.js
@@ -8,11 +8,10 @@ const updateGroupCentralLocation = (groupCoord, userCoord) => {
    const newLongitude = (groupCoord.st_x + userCoord.st_x) / 2;
    const newLatitude = (groupCoord.st_y + userCoord.st_y) / 2;
    return {
-        longitude: newLongitude,
-        latitude: newLatitude
+        st_x: newLongitude,
+        st_y: newLatitude
    }
 }
-
 
 const updateGroupInterests = async (groupInterests, userInterests) => {
     //this method will create a union set between group's existing interests and user's interests
@@ -23,5 +22,13 @@ const updateGroupInterests = async (groupInterests, userInterests) => {
     return [...unionInterests]; //return as array, not set object
 }
 
+const recalculateGroupCentralLocation = async (memberCoords) => {
+    let currGroupCoord = memberCoords.at(0)
+    for(let i = 1; i < memberCoords.length; i++) {
+        currGroupCoord = updateGroupCentralLocation(currGroupCoord, members[i].coord)
+    }
+    return currGroupCoord;
+}
 
-module.exports = { updateGroupCentralLocation, updateGroupInterests };
+
+module.exports = { updateGroupCentralLocation, updateGroupInterests, recalculateGroupCentralLocation };
