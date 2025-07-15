e2aa5ec5a7742b06f8f47c7b26994f8eeb9500fb
Erin Turgut <erintur@meta.com>
2025-07-14 20:14:50 -0700

    DLL, LRU, and ServerSide cache classes set up

---
+++ server/data_structures/DoublyLinkedList.js
@@ -0,0 +1,69 @@
+class DoublyLinkedList {
+
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+
+    getLength() {
+        return this.length;
+    }
+    
+    //beginning(head) will be considered most recent
+    insertBeginning(key, value) {
+        const newNode = new Node(key, value);
+        if (this.length === 0) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            newNode.prev = this.head;
+            this.head.next = newNode;
+            this.head = newNode;
+        }
+        this.length++;
+
+        return newNode;
+    }
+
+    //end(tail) will be considered least recent
+    removeEnd() {
+        let tempTail = this.tail;
+        if (this.length === 0) {
+            return null;
+        }
+        if (this.length == 1) {
+            this.head = null;
+            this.tail = null;
+        } else {
+            this.tail = this.tail.next;
+            this.tail.prev = null;
+            tempTail.next = null;
+        }
+        this.length--;
+        return tempTail;   
+    }
+
+    //used in debugging to see contents of DLL
+    getList() {
+        let dllArr = [];
+        let currNode = this.head;
+        while(currNode != null) {
+            dllArr.push({key: currNode.key,value: currNode.value})
+            currNode = currNode.prev;
+        }
+        return dllArr;
+    }
+}
+
+class Node {
+    constructor(key, value) {
+        this.key = key;
+        this.value = value;
+        this.next = null;
+        this.prev = null;
+    }
+}
+
+
+module.exports = {DoublyLinkedList};
