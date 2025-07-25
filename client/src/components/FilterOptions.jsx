a04e32b7474bb014830e67dcf97aff69a7f07f50
Erin Turgut <erintur@meta.com>
2025-07-25 10:05:12 -0700

    added to visually complex component

--- client/src/components/FilterOptions.jsx
+++ client/src/components/FilterOptions.jsx
@@ -6,28 +6,37 @@ const FilterOptions = ( {onFilterChange}) => {
 
     const [isFilterActive, setIsFilterActive] = useState(false);
     const [currHoveredFilter, setCurrHoveredFilter] = useState(0);
+    const [currSelectedFilter, setCurrSelectedFilter] = useState(Status.NONE);
 
     const handleFilterClick = (status) => {
+        setCurrSelectedFilter(status)
         onFilterChange(status)
     }
 
-    return <div className="filter-form" onMouseEnter={() => {setIsFilterActive(true)}} onMouseLeave={() => {setIsFilterActive(false)}}>
-        {!isFilterActive && <div className="filter-button" onClick={() => {setIsFilterActive(true)}}>Filter</div>}
-        {isFilterActive && 
-        <>
-            <div className="filter-option-container">
-                {Object.values(Status).map((status, index) => (
-                    <div className="filter-option" key={status} value={status} onMouseOver={() => {setCurrHoveredFilter(index)}} onClick={() => {handleFilterClick(status)}}>{status}</div>
-                ))}
-            </div>
-            <div className="dot-container">
-                {Object.values(Status).map((status, index) => (
-                    <div className={index === currHoveredFilter ? 'selected dot' : 'dot'} />
-                ))}
-            </div>
-        </>
-        }
+    return (
+    <div className="filter-component">
+        <div className="filter-form" onMouseEnter={() => {setIsFilterActive(true)}} onMouseLeave={() => {setIsFilterActive(false)}}>
+            {!isFilterActive && <div className="filter-button" onClick={() => {setIsFilterActive(true)}}>Filter</div>}
+            {isFilterActive && 
+            <>
+                <div className="filter-option-container">
+                    {Object.values(Status).map((status, index) => (
+                        <div className="filter-option" key={status} value={status} onMouseOver={() => {setCurrHoveredFilter(index)}} onClick={() => {handleFilterClick(status)}}>{status}</div>
+                    ))}
+                </div>
+                <div className="dot-container">
+                    {Object.values(Status).map((status, index) => (
+                        <div className={index === currHoveredFilter ? 'selected dot' : 'dot'} />
+                    ))}
+                </div>
+            </>
+            }
+        </div>
+       <div className={isFilterActive ? "hidden" : "curr-filter-display"}>
+            <div className="curr-filter-text">{currSelectedFilter}</div>
+        </div>
     </div>
+    )
 }
 
 export default FilterOptions;
