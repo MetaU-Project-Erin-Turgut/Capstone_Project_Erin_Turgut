360689cb7a76c9e20bdd9c01f0f942e899ee898a
Erin Turgut <erintur@meta.com>
2025-07-28 18:51:38 -0700

    styled profile page and added edit and save buttons

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -2,10 +2,15 @@ import { useState, useEffect } from "react";
 import APIUtils from "../utils/APIUtils";
 import NavBar from "../components/NavBar"
 import { DEFAULT_FORM_VALUE } from "../utils/utils";
+import { FaUserCircle } from "react-icons/fa";
+import { GrEdit } from "react-icons/gr";
+import { FaRegSave } from "react-icons/fa";
+import "../styles/ProfilePage.css";
 
 const ProfilePage = () => {
 
     const [formData, setFormData] = useState(DEFAULT_FORM_VALUE);
+    const [isEdit, setIsEdit] = useState(false);
 
     useEffect(() => {
         fetchUserInfo();
@@ -27,20 +32,73 @@ const ProfilePage = () => {
         }
     }
 
+    const handleInputChange = (event) => {
+        const { name, value } = event.target
+
+        setFormData((prevState) => ({
+            ...prevState,
+            [name]: value,
+        }))
+    }
+
+    const handleFormSubmit = () => {
+        //update user info in database
+        setIsEdit(false)
+    }
+
     return (
         <div id="profile-page">
             <NavBar isMenuVisible={false}/>
             <div className="page-header"><h2>Profile Page</h2></div>
-            <h3>First name:</h3>
-            <p>{formData.firstName}</p>
-            <h3>Last name:</h3>
-            <p>{formData.lastName}</p>
-            <h3>Username:</h3>
-            <p>{formData.username}</p>
-            <h3>Email:</h3>
-            <p>{formData.email}</p>
-            <h3>Address:</h3>
-            <p>{formData.address}</p>
+            <div className="profile-info">
+                <section className="image-side">
+                    <FaUserCircle className="profile-page-img"/>
+                </section>
+                <form className="info-section">
+                    <h3 className="input-label">First name:</h3>
+                    {!isEdit ? <p>{formData.firstName}</p> :
+                    <input className="profile-input"
+                            placeholder={formData.firstName}
+                            type="text"
+                            name="firstName"
+                            value={formData.firstName}
+                            onChange={handleInputChange}/>}
+                    <h3 className="input-label">Last name:</h3>
+                    {!isEdit ? <p>{formData.lastName}</p> :
+                    <input className="profile-input"
+                            placeholder={formData.lastName}
+                            type="text"
+                            name="lastName"
+                            value={formData.lastName}
+                            onChange={handleInputChange}/>}
+                    <h3 className="input-label">Username:</h3>
+                    {!isEdit ? <p>{formData.username}</p> :
+                    <input className="profile-input"
+                            placeholder={formData.username}
+                            type="text"
+                            name="username"
+                            value={formData.username}
+                            onChange={handleInputChange}/>}
+                    <h3 className="input-label">Email:</h3>
+                    {!isEdit ? <p>{formData.email}</p> :
+                    <input className="profile-input"
+                            placeholder={formData.email}
+                            type="text"
+                            name="email"
+                            value={formData.email}
+                            onChange={handleInputChange}/>}
+                    <h3 className="input-label">Address:</h3>
+                    {!isEdit ? <p>{formData.address}</p> :
+                    <input className="profile-input"
+                            placeholder={formData.address}
+                            type="text"
+                            name="address"
+                            value={formData.address}
+                            onChange={handleInputChange}/>}
+                    {isEdit ? <div className="save-btn" onClick={handleFormSubmit}><FaRegSave className="icon"/></div> : 
+                    <div className="edit-btn" onClick={() => {setIsEdit(true)}}><GrEdit className="icon"/></div>}
+                </form>
+            </div>
         </div>
     )
 }
